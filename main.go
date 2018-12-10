ab5b8c83f4ac2384ee98e1e8b0d96a172b288c6d
Admin <admin@Admins-MacBook-Pro.local>
2018-12-10 16:33:31 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello World",
+	})
+}
+func PostHomePage(c *gin.Context) {
+	body := c.Request.Body
+	value, err := ioutil.ReadAll(body)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	c.JSON(200, gin.H{
+		"message": string(value),
+	})
+}
+
+func QueryStrings(c *gin.Context) {
+	name := c.Query("name")
+	age := c.Query("age")
+
+	c.JSON(200, gin.H{
+		"name": name,
+		"age":  age,
+	})
+}
+func PathParameters(c *gin.Context) {
+	name := c.Param("name")
+	age := c.Param("age")
+
+	c.JSON(200, gin.H{
+		"name": name,
+		"age":  age,
+	})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", HomePage)
+	r.POST("/", PostHomePage)
+	r.GET("/query", QueryStrings)
+	r.GET("/path/:name/:age", PathParameters) // path/will/23/
+	r.Run()
+}
